a472819fd080ec7fd5c8707d9311e41af12eb027
Mohd Shahril <moharilstackup@gmail.com>
2018-10-30 23:10:36 +0800

    Fix the password validation between new password and confirm password at frontend

--- src/app/shared/security/change-password/change-password.component.ts
+++ src/app/shared/security/change-password/change-password.component.ts
@@ -4,7 +4,7 @@ import { SecurityService } from '../../services/security.service';
 import { MatSnackBar } from '@angular/material';
 import { User } from '../../models/user';
 import { PasswordValidation } from '../../validation/password-match';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -15,27 +15,28 @@ import { Router } from '@angular/router';
 export class ChangePasswordComponent implements OnInit {
   changePasswordForm: FormGroup;
   PASSWORD_PATTERN = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{12,}$/;
-  
+
   constructor(private securitySvc: SecurityService,
     private fb: FormBuilder,
     private snackSvc: MatSnackBar,
     private router: Router) {
-      this.changePasswordForm = fb.group({
-        currentPassword: ['', Validators.required],
-        password: ['', [Validators.required, Validators.pattern(this.PASSWORD_PATTERN)]],
-        confirmPassword: ['', Validators.required]
-      }, {
+    this.changePasswordForm = fb.group({
+      currentPassword: ['', Validators.required],
+      password: ['', [Validators.required, Validators.pattern(this.PASSWORD_PATTERN)]],
+      confirmPassword: ['', Validators.required]
+    }, {
         validator: [PasswordValidation.MatchPassword]
-      }) 
+      })
 
-    }
+  }
 
   ngOnInit() {
   }
 
-  onSubmit(){
+  onSubmit() {
     let confirmPassword = this.changePasswordForm.get("confirmPassword").value;
-    
+
+
     let updateUser: User = {
       id: this.securitySvc.getCurrentUser().id,
       email: this.securitySvc.getCurrentUser().email,
@@ -43,15 +44,24 @@ export class ChangePasswordComponent implements OnInit {
       newpassword: confirmPassword
     }
     console.log(this.changePasswordForm);
+    console.log("Confirm password :", this.changePasswordForm.controls['confirmPassword'].hasError('MatchPassword'));
+    //console.log("Validator  : ", this.changePasswordForm.validator);
     ///first hash to the server side
-    this.securitySvc.changePassword(updateUser).subscribe((result)=>{
-      console.log(result);
-      this.securitySvc.logout(); //force logout
-      this.router.navigate(['/Login']);
-      let snackBarRef = this.snackSvc.open("Password Updated. Please Log In Again!", 'Done', {
-        duration: 3000
+    if (this.changePasswordForm.controls['confirmPassword'].hasError('MatchPassword')) {  //mismatch
+      let snackBarRef = this.snackSvc.open("Password mismatch. Please try Again!", 'Done', {
+        duration: 5000
       });
-    })
+      //if still fail, may want to try to reset the change password form
+    } else {
+      this.securitySvc.changePassword(updateUser).subscribe((result) => {
+        console.log(result);
+        this.securitySvc.logout(); //force logout
+        this.router.navigate(['/Login']);
+        let snackBarRef = this.snackSvc.open("Password Updated. Please Log In Again!", 'Done', {
+          duration: 3000
+        });
+      })
+    }
   }
 
 }
